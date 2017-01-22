6d3e27d8b38edc09a63f6021ad7d4ddb09b81818
Joe Bosman <joseph.a.bosman@gmail.com>
2017-01-22 16:52:05 -0500

    Added classInfo and rates sections

---
+++ browser/js/classInfo/classInfo.js
@@ -0,0 +1,6 @@
+app.config( $stateProvider => {
+	return $stateProvider.state('classInfo', {
+		url: '/class-information',
+		templateUrl: 'js/classInfo/classInfo.html'
+	})
+})
---
+++ browser/js/rates/rates.js
@@ -0,0 +1,6 @@
+app.config( $stateProvider => {
+	return $stateProvider.state('rates', {
+		url: '/rates',
+		templateUrl: 'js/rates/rates.html'
+	});
+});
