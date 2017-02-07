22fdb7521c120dcb37955f5cd2aa0987e1d8a990
Joe Bosman <joseph.a.bosman@gmail.com>
2017-02-07 12:51:15 -0500

    Switching gears to flowing page app instead after meating with Sheldon

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,9 +1,16 @@
-app.config(function ($stateProvider) {
+// app.config(function ($stateProvider) {
 
-    // Register our *about* state.
-    $stateProvider.state('about', {
-        url: '/about',
-        templateUrl: 'js/about/about.html'
-    });
+//     // Register our *about* state.
+//     $stateProvider.state('about', {
+//         url: '/about',
+//         templateUrl: 'js/about/about.html'
+//     });
 
+// });
+
+app.directive('about', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/about/about.html'
+	}
 });
