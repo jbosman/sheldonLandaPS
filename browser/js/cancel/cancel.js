955a9172bf0a44a90b20c7a5d2e9ce051566e428
Joe Bosman <joseph.a.bosman@gmail.com>
2017-02-07 16:26:08 -0500

    Updated cancellation policy section

--- browser/js/cancel/cancel.js
+++ browser/js/cancel/cancel.js
@@ -1,6 +1,15 @@
-app.config( $stateProvider => {
-	return $stateProvider.state('cancel', {
-		url: '/cancelation-policy',
+// app.config( $stateProvider => {
+// 	return $stateProvider.state('cancel', {
+// 		url: '/cancelation-policy',
+// 		templateUrl: 'js/cancel/cancel.html'
+// 	})
+// })
+
+app.directive('cancelPolicy', function(){
+
+	return {
+		restrict: 'E',
 		templateUrl: 'js/cancel/cancel.html'
-	})
-})
+	}
+
+});
