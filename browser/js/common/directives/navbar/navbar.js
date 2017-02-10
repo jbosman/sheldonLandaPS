d1042ae171b1fbe5f0919a2fd3b27455425796bc
Joe Bosman <joseph.a.bosman@gmail.com>
2017-02-10 16:28:02 -0500

    Updated with big piano pick so sheldon can see it

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,13 +7,13 @@ app.directive('navbar', function () {
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', href: '#home', deskId: 'ataghomeDesk', mobileId:  'ataghomeMobile' },
+                { label: 'Welcome', href: '#home', deskId: 'ataghomeDesk', mobileId:  'ataghomeMobile' },
                 { label: 'About', href: '#about', deskId: 'atagaboutDesk', mobileId:  'atagaboutMobile'},
                 { label: 'Class Info', href: '#classInfo', deskId: 'atagclassInfoDesk', mobileId:  'atagclassInfoMobile' },
                 { label: 'Rates', href: '#rates', deskId: 'atagratesDesk', mobileId:  'atagratesMobile' },
                 { label: 'Cancellations', href: '#cancel', deskId: 'atagcancelDesk', mobileId:  'atagcancelMobile' },
                 { label: 'Location', href: '#location', deskId: 'ataglocationDesk', mobileId:  'ataglocationMobile' },
-                { label: 'Light Painting', href: '#lightPainting', deskId: 'ataglightPaintingDesk', mobileId:  'ataglightPaintingMobile' },
+                { label: 'Reviews', href: '#reviews', deskId: 'atagreviewsDesk', mobileId:  'atagreviewsMobile' },
                 { label: 'Music', href: '#music', deskId: 'atagmusicDesk', mobileId:  'atagmusicMobile' },
             ];
 
