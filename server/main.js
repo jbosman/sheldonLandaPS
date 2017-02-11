f1239e1a77e0f0def9a9c4c82481e78f3780f99a
Joe Bosman <joseph.a.bosman@gmail.com>
2017-02-11 18:58:40 -0500

    Updated server with self crawl to keep up on heroku.

--- server/main.js
+++ server/main.js
@@ -3,6 +3,17 @@ let chalk = require('chalk');
 
 // Create a node server instance! cOoL!
 let server = require('http').createServer();
+let RequestPromise = require('request-promise');
+
+function selfCrawl(){
+
+	RequestPromise('http://www.sheldonlanda.com')
+	.then( () => {
+		console.log('selfcrawl succeeded...');
+	})
+	.catch(console.error);
+
+}
 
 let createApplication = function () {
     let app = require('./app')();
@@ -21,3 +32,4 @@ let startServer = function () {
 
 createApplication();
 startServer();
+setInterval( selfCrawl, 1500000); // Keep heroku site up
