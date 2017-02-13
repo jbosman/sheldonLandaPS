1b2e514a172561d5eeac923ec67543db35d33b52
Joe Bosman <joseph.a.bosman@gmail.com>
2017-02-13 15:57:56 -0500

    Updated with Sheldon's requests from Feb 13 meeting.

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,13 +7,13 @@ app.directive('navbar', function () {
         link: function (scope) {
 
             scope.items = [
-                { label: 'Welcome', href: '#home', deskId: 'ataghomeDesk', mobileId:  'ataghomeMobile' },
-                { label: 'About', href: '#about', deskId: 'atagaboutDesk', mobileId:  'atagaboutMobile'},
+                { label: 'Welcome', href: '#welcome', deskId: 'atagwelcomeDesk', mobileId:  'atagwelcomeMobile' },
                 { label: 'Class Info', href: '#classInfo', deskId: 'atagclassInfoDesk', mobileId:  'atagclassInfoMobile' },
+                { label: 'Location', href: '#location', deskId: 'ataglocationDesk', mobileId:  'ataglocationMobile' },
                 { label: 'Rates', href: '#rates', deskId: 'atagratesDesk', mobileId:  'atagratesMobile' },
                 { label: 'Cancellations', href: '#cancel', deskId: 'atagcancelDesk', mobileId:  'atagcancelMobile' },
-                { label: 'Location', href: '#location', deskId: 'ataglocationDesk', mobileId:  'ataglocationMobile' },
                 // { label: 'Reviews', href: '#reviews', deskId: 'atagreviewsDesk', mobileId:  'atagreviewsMobile' },
+                { label: 'About', href: '#about', deskId: 'atagaboutDesk', mobileId:  'atagaboutMobile'},
                 { label: 'Music', href: '#music', deskId: 'atagmusicDesk', mobileId:  'atagmusicMobile' },
             ];
 
